f0ded252ad760642c4525a35f0a4736230c021a4
K-Walker-NNK <kwalker.nnk@gmail.com>
2022-05-16 14:58:35 +0700

    khanhnguyen

--- screens/MyMusic.js
+++ screens/MyMusic.js
@@ -5,8 +5,8 @@ import { View, Text, Button, StyleSheet} from 'react-native';
 const MyMusic = ({navigation, route}) => {
     return(
         <View style={styles.container}>
-            <Text>Bài hát {route.params.nameMusic} </Text>
-            <Text>My Music {route.params.idMusic} </Text>
+            <Text>Bài hát {route.params?.nameMusic} </Text>
+            <Text>My Music {route.params?.idMusic} </Text>
             <Button
             title='Click me'
             onPress={()=>alert('Button Clicked!')}
